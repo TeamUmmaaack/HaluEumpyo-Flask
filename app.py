d6390fed9e476b7ff26d79bee87cbb071bf3b2a4
bsorinnn <bxxsxrxx@gmail.com>
2022-05-22 21:55:15 +0900

    Heroku~~~~

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-import numpy as np
+import os
 import json
+import torch
+from torch import nn
 from flask import Flask, request, jsonify
-from sentiment_analysis_koBERT import predict
-
+from werkzeug.exceptions import BadRequest
+import sys
+from os import path
 
 app = Flask(__name__)
 
-
 @app.route('/test', methods=['POST'])
 def test():
-    content = json.loads(request.get_data(), encoding='utf-8')
-    print(content)
-    data = predict(content)
+    content = json.loads(request.get_data('content'))
+    print(content['content'])
+    sys.path.append(path.join(path.dirname(__file__)))
+    from sentiment_analysis_koBERT import predict
+    data = predict(content['content'])
     return jsonify({
         'emotion_id': data
     })
@@ -20,4 +24,4 @@ def test():
 if __name__ == '__main__':
     PORT = 50051
 
-    app.run(host="0.0.0.0", debug=True, port=PORT)
+    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get("PORT", 5000)))
--- sentiment_analysis_koBERT.py
+++ sentiment_analysis_koBERT.py
@@ -1,17 +1,40 @@
-# torch
-from kobert.pytorch_kobert import get_pytorch_kobert_model
-import torch
+import gluonnlp as nlp
 import numpy as np
+import torch
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+from kobert.utils.utils import get_tokenizer
+from torch.utils.data import Dataset
+
 from Bert_model import BERTClassifier
-from dataset import BERTDataset, tok
 
-# kobert
+kobert_model, vocab = get_pytorch_kobert_model()
+model = BERTClassifier(kobert_model, dr_rate=0.5, num_classes=4)
 
-# GPU 사용
-device = torch.device("cuda:0")
+ctx = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(ctx)
 
-# BERT 모델 불러오기 필수
-bertmodel = get_pytorch_kobert_model()
+emotion_clsf_weights_file = "data/model_state_dict.pt"
+model.load_state_dict(torch.load(
+    emotion_clsf_weights_file, map_location=device))
+model.eval()
+
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len, pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return self.sentences[i] + (self.labels[i],)
+
+    def __len__(self):
+        return len(self.labels)
 
 
 # Setting parameters
@@ -23,11 +46,6 @@ max_grad_norm = 1
 log_interval = 200
 learning_rate = 5e-5
 
-# 학습 모델 로드
-model = BERTClassifier(bertmodel,  dr_rate=0.5).to(device)
-model = torch.load('data/KoBERT_model.pt')
-model.load_state_dict(torch.load('data/model_state_dict.pt'))
-
 
 def new_softmax(a):
     c = np.max(a)
@@ -38,63 +56,50 @@ def new_softmax(a):
 
 
 # 예측 모델 설정
-
-
 def predict(predict_sentence):
-
     data = [predict_sentence, '0']
     dataset_another = [data]
 
     another_test = BERTDataset(
         dataset_another, 0, 1, tok, max_len, True, False)
     test_dataloader = torch.utils.data.DataLoader(
-        another_test, batch_size=batch_size, num_workers=5)
+        another_test, batch_size=batch_size, num_workers=0)
 
     model.eval()
 
     for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
         token_ids = token_ids.long().to(device)
         segment_ids = segment_ids.long().to(device)
+
         valid_length = valid_length
         label = label.long().to(device)
 
         out = model(token_ids, valid_length, segment_ids)
 
-    test_eval = []
-    emotion_arr = []
-    for i in out:
-        logits = i
-        logits = logits.detach().cpu().numpy()
-        min_v = min(logits)
-        total = 0
-        probability = []
-        logits = np.round(new_softmax(logits), 3).tolist()
-        for logit in logits:
-            # print(logit)
-            probability.append(np.round(logit, 3))
+        test_eval = []
+        emotion_arr = []
+        for i in out:
+            logits = i
+            logits = logits.detach().cpu().numpy()
 
-        if np.argmax(logits) == 0:
-            test_eval.append("행복이")
-            emotion = "행복"
-            emotion_arr.append(1)
-
-        elif np.argmax(logits) == 1:
-            test_eval.append("슬픔이")
-            emotion = "슬픔"
-            emotion_arr.append(2)
-
-        elif np.argmax(logits) == 2:
-            test_eval.append("분노가")
-            emotion = "분노"
-            emotion_arr.append(4)
-
-        elif np.argmax(logits) == 3:
-            test_eval.append("특정한 감정이 안")
-            emotion = "중립"
-            emotion_arr.append(7)
-
-        probability.append(emotion)
-    # print(probability)
-
-    print(">> 일기에서 " + test_eval[0] + " 느껴집니다.")
-    return emotion_arr[0]
+            if np.argmax(logits) == 0:
+                test_eval.append("행복이")
+                emotion = "행복"
+                emotion_arr.append(1)
+            elif np.argmax(logits) == 1:
+                test_eval.append("슬픔이")
+                emotion = "슬픔"
+                emotion_arr.append(2)
+            elif np.argmax(logits) == 2:
+                test_eval.append("분노가")
+                emotion = "분노"
+                emotion_arr.append(4)
+            elif np.argmax(logits) == 3:
+                test_eval.append("특정한 감정이 안")
+                emotion = "중립"
+                emotion_arr.append(7)
+            else:
+                test_eval.append("안녕")
+            # print(probability)
+        print(">> 일기에서 " + test_eval[0] + " 느껴집니다.")
+        return emotion_arr[0]
