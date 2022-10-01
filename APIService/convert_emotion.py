e6dd40bbe3fa2168918eec7cf8e6db96ac07697c
bsorinnn <bxxsxrxx@gmail.com>
2022-10-01 19:17:56 +0900

    [ADD] 혐오, 놀람, 공포 감정 추가 (#11)

--- APIService/convert_emotion.py
+++ APIService/convert_emotion.py
@@ -5,5 +5,11 @@ def convert_emotion(emotion_id):
         return 2
     elif emotion_id == 2:  # 분노
         return 4
-    elif emotion_id == 3:  # 휴식
+    elif emotion_id == 3:  # 중립
         return 7
+    elif emotion_id == 4:  # 혐오
+        return 5
+    elif emotion_id == 5:  # 놀람
+        return 3
+    else:  # 공포
+        return 6
